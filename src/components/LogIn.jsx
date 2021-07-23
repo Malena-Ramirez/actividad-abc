4c8ce3e4845d63cddaf1d6f474ed40265a4c12a5
Malena Ramirez <maramirezuribe@gmail.com>
2021-07-23 10:00:29 -0500

    Estructura inicial del login

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { useForm } from '../hooks/useForm';
+import { login } from '../actions/action';
+
+const LogIn = () => {
+  const dispatch = useDispatch();
+  const [formValues, handleInputChange] = useForm({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formValues;
+
+  const handleLogIn = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={handleLogIn}>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Label>Correo electrónico</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Correo electrónico'
+            name='email'
+            value={email}
+            onChange={handleInputChange}
+          />
+          <Form.Text className='text-muted'></Form.Text>
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Contraseña'
+            name='password'
+            value={password}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <div className='d-flex justify-content-center'>
+          <Button size='lg' variant='info' type='submit'>
+            Iniciar Sesión
+          </Button>
+        </div>
+        <Container className='auth__social-networks'>
+          <Container className='google-btn'>
+            <Container className='google-icon-wrapper'>
+              <img
+                className='google-icon'
+                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
+                alt='googlebutton'
+              />
+            </Container>
+          </Container>
+        </Container>
+      </Form>
+    </Container>
+  );
+};
+
+export default LogIn;
---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Products from '../components/Products';
+import LogIn from '../components/LogIn';
+
+function App() {
+  return (
+    <div className="App">
+      <Products />
+      <LogIn />
+    </div>
+  );
+}
+
+export default App;
